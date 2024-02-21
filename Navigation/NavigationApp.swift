c60647edf2f871190f81bb46cfb9c0753db49aae
Izaan Saleem <izaan.saleem@m3tech.com.pk>
2024-02-21 15:49:15 +0500

    first commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Izaan Saleem on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
