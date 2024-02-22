ce5199c1fa079a8fdb88812481c9665b0b435c41
Izaan Saleem <izaan.saleem@m3tech.com.pk>
2024-02-22 12:03:12 +0500

    day 43/100 completed

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -7,15 +7,45 @@
 
 import SwiftUI
 
+struct DetailView: View {
+    var number: Int
+
+    var body: some View {
+        Text("Detail View \(number)")
+    }
+
+    init(number: Int) {
+        self.number = number
+        print("Creating detail view \(number)")
+    }
+}
+
+struct Student: Hashable {
+    var id = UUID()
+    var name: String
+    var age: Int
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(0..<4) { i in
+                NavigationLink("Select \(i)", value: i)
+            }
+            .navigationDestination(for: Int.self) { selection in
+                Text("You selected: \(selection)")
+            }
+            .navigationDestination(for: Student.self) { student in
+                Text("You selected: \(student.name)")
+            }
+            NavigationLink("Tap Me") {
+                List(0..<1000) { i in
+                    NavigationLink("Tap Me") {
+                        DetailView(number: i)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
